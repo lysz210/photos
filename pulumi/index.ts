5146d3da4abbde1a9d2bf8f22595029f0bdc2966
Lingyong Sun <lingyong.sun@gmail.com>
2025-01-21 21:57:28 +0100

    LYS-24 add gh-pages domain configuration

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,8 +1,37 @@
 import { BucketV2 } from "@pulumi/aws/s3";
-
+import {getZone, Record, RecordType} from "@pulumi/aws/route53";
+import { Config } from "@pulumi/pulumi";
+const config = new Config('photos')
 // create photo repositories
 // flickr
 const flickrRepo = new BucketV2("my-flickr-repo");
 
+const lysz210Zone = getZone({
+    name: "lysz210.name"
+})
+
+const ghPagesVerificationRecord = new Record("ghPagesVerificationRecord", {
+    zoneId: lysz210Zone.then(zone => zone.zoneId),
+    type: RecordType.TXT,
+    name: config.requireSecret<string>("ghVerificationHostname"),
+    records: [config.requireSecret<string>("ghVerificationValue")],
+    ttl: 300
+});
+
+const ghPagesPointerRecord = new Record("ghPagesPointerRecord", {
+    zoneId: lysz210Zone.then(zone => zone.zoneId),
+    type: RecordType.A,
+    name: 'photos.gh.lysz210.name',
+    records: [
+        '185.199.108.153',
+        '185.199.109.153',
+        '185.199.110.153',
+        '185.199.111.153',
+    ],
+    ttl: 300
+})
+
 // Export the name of the bucket
 export const flickrRepoId = flickrRepo.id;
+export const ghPagesVerification = ghPagesVerificationRecord.name
+export const ghPagesPointer = ghPagesPointerRecord.name
